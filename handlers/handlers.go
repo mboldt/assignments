200c673aa85cf90c23f9b04b9dd2c33345ffedbb
Mikey Boldt <mike.boldt@gmail.com>
2024-12-09 19:42:25 +0000

    wip

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,14 +1,40 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/mboldt/assignments/datastore"
+	"github.com/mboldt/assignments/student"
 	"github.com/mboldt/assignments/templates"
 )
 
-type Handler struct{}
+type Handler struct {
+	ds *datastore.Datastore
+}
 
-func (h Handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+func New(ds *datastore.Datastore) *Handler {
+	return &Handler{ds: ds}
+}
+
+func (h *Handler) Index(w http.ResponseWriter, _ *http.Request) {
 	renderer := templates.NewRenderer("templates")
-	renderer.Render(w, "index.html", nil)
+	renderer.Render(w, "index.html", h.ds)
+}
+
+func (h *Handler) AddStudentGet(w http.ResponseWriter, _ *http.Request) {
+	renderer := templates.NewRenderer("templates")
+	renderer.Render(w, "add-student.html", nil)
+}
+
+func (h *Handler) AddStudentPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("add student post error: %s", err)
+		http.Error(w, "error reading form", 400)
+	}
+	log.Printf("received form data %v", r.Form)
+	log.Printf("received body %v", r.Body)
+	h.ds.AddStudent(student.New(r.Form.Get("name")))
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
--- main.go
+++ main.go
@@ -4,13 +4,32 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/mboldt/assignments/datastore"
 	"github.com/mboldt/assignments/handlers"
 )
 
-func init() {
-	http.Handle("/", handlers.Handler{})
+type app struct {
+	ds *datastore.Datastore
+}
+
+func middleware(f func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("path: %s", r.URL.Path)
+		f(w, r)
+	})
+}
+
+func (a *app) init() {
+	handler := handlers.New(a.ds)
+	http.Handle("/{$}", middleware(handler.Index))
+	http.Handle("GET /add-student", middleware(handler.AddStudentGet))
+	http.Handle("POST /add-student", middleware(handler.AddStudentPost))
 }
 
 func main() {
+	app := &app{
+		ds: datastore.New(),
+	}
+	app.init()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
